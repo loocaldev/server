86fcd13d3a851219e0de4377f679e780ed682dd0
loocaldev <loocaldigital@gmail.com>
2024-06-03 20:10:03 -0500

    third

--- loocal/settings.py
+++ loocal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$got=fmejf+82uz^qswc2&i^hqc=srnz$-(@h*$0kw70nmukwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
