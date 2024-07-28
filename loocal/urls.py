eaddc8c92c8c4e9047f76659a4db92e386f79d65
loocaldev <loocaldigital@gmail.com>
2024-07-28 12:06:21 -0500

    thir adjust static2}

--- loocal/urls.py
+++ loocal/urls.py
@@ -37,5 +37,5 @@ urlpatterns = [
     path('update_user/', views.update_user, name='update_user'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
