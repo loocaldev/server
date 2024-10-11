076fd7cc8167a300f14d6d5627016a412695bc09
loocaldev <loocaldigital@gmail.com>
2024-10-11 17:03:09 -0500

    adjust auth

--- loocal/settings.py
+++ loocal/settings.py
@@ -1,28 +1,23 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
 import datetime
+import requests  # Asegurarse de que esta librería esté instalada
+import jwt  # Asegurarse de tener la librería pyjwt instalada
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, "webappexample", "templates")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['44.220.218.144', 'loocal.co', 'www.loocal.co']
 
-
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'rest_framework.authtoken',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -55,18 +50,18 @@ ROOT_URLCONF = 'loocal.urls'
 
 AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN', 'dev-bgodgyzz8ugzloem.us.auth0.com')
 API_IDENTIFIER = os.getenv('API_IDENTIFIER', 'https://dev-bgodgyzz8ugzloem.us.auth0.com/api/v2/')
+ALGORITHMS = ["RS256"]
 
-# Configuración de REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # Puedes eliminar JWT si no lo usas para otras funcionalidades
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
-# Añade esta configuración para obtener las claves públicas de Auth0 y validar los JWT
+# Añadimos la configuración de las claves públicas de Auth0 para validar los JWT
 def get_jwks():
     jwks_url = f"https://{AUTH0_DOMAIN}/.well-known/jwks.json"
     jwks = requests.get(jwks_url).json()
@@ -105,15 +100,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'loocal.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -124,9 +110,6 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT', '5432'), 
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -143,10 +126,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Bogota'
@@ -157,10 +136,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -170,12 +145,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Media files (Uploaded by users)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -187,14 +158,6 @@ CORS_ALLOWED_ORIGINS = [
     'http://loocal.co',
     'http://44.220.218.144',
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
-}
 
 SESSION_COOKIE_AGE = 3600
 
@@ -212,9 +175,6 @@ CSP_OBJECT_SRC = ("'self'",)
 
 CSRF_TRUSTED_ORIGINS = [
     'http://*',
-    'https://server-production-1ddc.up.railway.app',
-    'http://107.20.105.251',
-    'http://ec2-107-20-105-251.compute-1.amazonaws.com',
     'https://loocal.co',
     'http://loocal.co',
     'http://44.220.218.144',
@@ -228,7 +188,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'camilo@loocal.co'
 EMAIL_HOST_PASSWORD = 'L00c4l@dev24'
-EMAIL_USE_TLS = True 
 DEFAULT_FROM_EMAIL = 'your_email@example.com'
 
 DEBUG = True
--- loocal/views.py
+++ loocal/views.py
@@ -1,19 +1,36 @@
-# views.py
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from django.contrib.auth import authenticate, login as django_login
 from rest_framework.response import Response
-from .serializers import UserSerializer, UserProfileSerializer, AddressSerializer
 from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-from .models import UserProfile, Address
-import datetime
-from . import signals
-
+from .serializers import UserSerializer
+from .models import UserProfile
+import jwt
+from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
+import requests
+
+# Obtener el conjunto de claves de Auth0 (JWKS)
+def get_jwks():
+    jwks_url = f"https://{settings.AUTH0_DOMAIN}/.well-known/jwks.json"
+    jwks = requests.get(jwks_url).json()
+    return jwks
+
+# Extraer la clave RSA adecuada para verificar el token JWT
+def get_rsa_key(token):
+    jwks = get_jwks()  # Obtener claves
+    unverified_header = jwt.get_unverified_header(token)  # Decodificar el encabezado sin verificar el token
+
+    rsa_key = {}
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:  # Coincidir por 'kid'
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    return rsa_key
 
 # Verificar el JWT proporcionado por Auth0
 def decode_auth0_token(token):
@@ -29,7 +46,7 @@ def decode_auth0_token(token):
         return payload
     raise ValueError('No se pudo obtener la clave RSA de Auth0')
 
-# Obtener o crear el usuario basado en la autenticación de Auth0
+# Sincronizar el usuario de Auth0 con el modelo de Django
 @api_view(['GET'])
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
@@ -43,140 +60,23 @@ def profile(request):
     except jwt.JWTClaimsError:
         return Response({'error': 'Token inválido'}, status=400)
 
-    # Sincronizar el usuario en Django usando la información de Auth0
+    # Crear o actualizar el usuario en Django basado en la información de Auth0
     user, created = User.objects.get_or_create(
-        username=payload['sub'],  # Auth0 sub
+        username=payload['sub'],  # Auth0 "sub" es el identificador único
         defaults={'email': payload.get('email', '')}
     )
 
-    # Actualizar la información del usuario si es necesario
     user.email = payload.get('email', user.email)
     user.save()
 
     # Crear o actualizar el perfil del usuario
     profile_data = {'profile_picture': payload.get('picture', None)}
     profile, _ = UserProfile.objects.get_or_create(user=user, defaults=profile_data)
-    
+
     if payload.get('picture'):
         profile.profile_picture = payload.get('picture')
         profile.save()
 
-    # Retornar los datos del usuario en Django
+    # Retornar los datos del usuario
     user_serializer = UserSerializer(instance=user)
     return Response(user_serializer.data)
-
-@api_view(['POST'])
-def login(request):
-    user = authenticate(username=request.data['username'], password=request.data['password'])
-    
-    if not user.check_password(request.data['password']):
-        return Response({"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
-    
-    django_login(request, user)
-    
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(instance=user)
-    
-    print("Login: Session cookie age:", request.session.get_expiry_age())
-    print("Login: Token expiration:", token.created + datetime.timedelta(hours=1))
-    
-    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def register(request):
-    user_serializer = UserSerializer(data=request.data)
-    
-    if user_serializer.is_valid():
-        user = user_serializer.save()
-        user.email = request.data['username']
-        user.set_password(request.data['password'])
-        user.save()
-
-        profile_data = request.data.get('profile', {})
-        UserProfile.objects.create(user=user, **profile_data)
-        
-        addresses_data = request.data.get('addresses', [])
-        for address_data in addresses_data:
-            Address.objects.create(user=user, **address_data)
-        
-        token = Token.objects.create(user=user)
-        return Response({'token': token.key, "user": user_serializer.data}, status=status.HTTP_201_CREATED)
-    
-    return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def add_address(request):
-    user = request.user
-    address_serializer = AddressSerializer(data=request.data)
-    
-    if address_serializer.is_valid():
-        address_serializer.save(user=user)
-        return Response(address_serializer.data, status=status.HTTP_201_CREATED)
-    
-    return Response(address_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def get_addresses(request):
-    user = request.user
-    addresses = Address.objects.filter(user=user)
-    serializer = AddressSerializer(instance=addresses, many=True)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def logout(request):
-    token = Token.objects.get(user=request.user)
-    token.delete()
-    return Response("Logout successful", status=status.HTTP_200_OK)
-
-@api_view(['PATCH'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def update_user(request):
-    user = request.user  # Obtener el objeto User asociado al usuario actual
-
-    # Recopilar los datos que deseas actualizar desde la solicitud
-    new_email = request.data.get('email')
-    new_password = request.data.get('password')
-    new_first_name = request.data.get('first_name')
-    new_last_name = request.data.get('last_name')
-
-    # Actualizar los campos necesarios del objeto User
-    if new_email:
-        user.email = new_email
-        user.username = new_email
-    if new_password:
-        user.set_password(new_password)
-    if new_first_name:
-        user.first_name = new_first_name
-    if new_last_name:
-        user.last_name = new_last_name
-
-    # Guardar los cambios en la base de datos
-    user.save()
-
-    return Response("User updated successfully", status=status.HTTP_200_OK)
-
-@api_view(['PATCH'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def update_address(request, pk):
-    address = get_object_or_404(Address, pk=pk, user=request.user)
-    serializer = AddressSerializer(instance=address, data=request.data, partial=True)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['DELETE'])
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def delete_address(request, pk):
-    address = get_object_or_404(Address, pk=pk, user=request.user)
-    address.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
