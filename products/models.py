d76fa4a7a30040957caba9d5ae3e630d93971a7e
loocaldev <loocaldigital@gmail.com>
2024-10-18 21:43:10 -0500

    Add categories for products app

--- products/models.py
+++ products/models.py
@@ -7,8 +7,15 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     unit = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=4)
+    categories = models.CharField(max_length=255, blank=True)  # Campo de categorías
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now=True)
     
     def __str__(self):
         return self.name
+    
+    def get_categories_list(self):
+        return [cat.strip() for cat in self.categories.split(',') if cat.strip()]
+
+    
+    
--- products/serializer.py
+++ products/serializer.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    categories = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_categories(self, obj):
+        return obj.get_categories_list()  # Devuelve las categorías como una lista
