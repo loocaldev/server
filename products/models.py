2cbb967dea928fb9adf5e5cf225677fb03c1c69c
loocaldev <loocaldigital@gmail.com>
2024-10-19 14:37:38 -0500

    adjust variable

--- products/models.py
+++ products/models.py
@@ -14,18 +14,20 @@ class Category(models.Model):
     def product_count(self):
         return self.products.count()
 
-# Producto principal
 class Product(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='product_images/', null=True, blank=True)
     description = models.TextField(blank=True)
     unit = models.TextField(blank=True)
-    categories = models.ManyToManyField('Category', related_name='products')  # Relación con categorías
+    price = models.DecimalField(max_digits=10, decimal_places=4)  # Precio fijo si no es variable
+    categories = models.ManyToManyField('Category', related_name='products')
+    is_variable = models.BooleanField(default=False)  # Define si el producto tiene variaciones
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
+
 
 # Atributos dinámicos, como "estado de maduración"
 class Attribute(models.Model):
