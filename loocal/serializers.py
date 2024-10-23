4934f373872446b40fd0bbdbec9bac1b3cbd8a38
loocaldev <loocaldigital@gmail.com>
2024-10-22 21:45:15 -0500

    adjustsss

--- loocal/serializers.py
+++ loocal/serializers.py
@@ -21,7 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'userprofile', 'addresses']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'userprofile', 'addresses']
         
 
     def update(self, instance, validated_data):
