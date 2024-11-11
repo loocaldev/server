fae7fd708d4b8cd691f23901269a58865e458551
loocaldev <loocaldigital@gmail.com>
2024-11-10 21:55:40 -0500

    adjust api apply discountsss

--- orders/urls.py
+++ orders/urls.py
@@ -3,13 +3,12 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from orders import views
 
-# Configura el router sin prefijo adicional
 router = routers.DefaultRouter()
-router.register(r'', views.OrderView, basename='order')
+router.register(r'order', views.OrderView, basename='order')
 
 urlpatterns = [
-    path('customid/<str:custom_order_id>/', views.OrderByCustomOrderIdAPIView.as_view(), name='order-by-custom-order-id'),
-    path('', include(router.urls)),  # Incluye el router sin prefijo adicional
     path('apply-discount/', views.apply_discount, name='apply-discount'),
+    path('customid/<str:custom_order_id>/', views.OrderByCustomOrderIdAPIView.as_view(), name='order-by-custom-order-id'),
+    path('', include(router.urls)),  # Las rutas de OrderView ahora estarán bajo 'order/'
     path("docs/", include_docs_urls(title="Orders API")),
 ]
