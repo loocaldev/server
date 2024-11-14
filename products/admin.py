3c6666884cfedc738f0dc6e3dd99341b12f32f19
loocaldev <loocaldigital@gmail.com>
2024-11-13 22:20:08 -0500

    adjust upload

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,10 @@
 # admin.py
 from django.contrib import admin
 from .models import Product, Category, Attribute, AttributeOption, ProductVariation, UnitType, UnitTypeAggregation
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
+# Inline para opciones de atributos en el administrador de atributos
 class AttributeOptionInline(admin.TabularInline):
     model = AttributeOption
     extra = 1
@@ -9,6 +12,7 @@ class AttributeOptionInline(admin.TabularInline):
 class AttributeAdmin(admin.ModelAdmin):
     inlines = [AttributeOptionInline]
 
+# Inline para variaciones de producto en el administrador de productos
 class ProductVariationInline(admin.TabularInline):
     model = ProductVariation
     extra = 1
@@ -18,10 +22,37 @@ class ProductVariationInline(admin.TabularInline):
     ]
     readonly_fields = ['final_price']
 
-class ProductAdmin(admin.ModelAdmin):
+# Recursos para importación/exportación de productos
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+        fields = (
+            'name', 'description', 'price', 'is_variable', 'unit_type__name', 
+            'unit_quantity', 'categories__name', 'is_on_promotion', 
+            'discount_type', 'discount_value', 'image'
+        )
+
+class ProductVariationResource(resources.ModelResource):
+    product_name = resources.Field(attribute='product__name', column_name='product_name')
+
+    class Meta:
+        model = ProductVariation
+        fields = (
+            'product_name', 'sku', 'price', 'stock', 'unit_type__name', 
+            'unit_quantity', 'is_on_promotion', 'discount_type', 'discount_value', 
+            'image', 'attribute_options__name'
+        )
+
+# Configuración del administrador de productos
+@admin.register(Product)
+class ProductAdmin(ImportExportModelAdmin):
+    resource_class = ProductResource
     inlines = [ProductVariationInline]
 
-class ProductVariationAdmin(admin.ModelAdmin):
+# Configuración del administrador de variaciones de productos
+@admin.register(ProductVariation)
+class ProductVariationAdmin(ImportExportModelAdmin):
+    resource_class = ProductVariationResource
     list_display = ('sku', 'product', 'price', 'final_price', 'is_on_promotion', 'discount_type', 'discount_value', 'stock')
     list_filter = ('is_on_promotion', 'discount_type')
     readonly_fields = ['final_price']
@@ -29,14 +60,9 @@ class ProductVariationAdmin(admin.ModelAdmin):
         'product', 'sku', 'price', 'stock', 'image', 'attribute_options', 'unit_type', 
         'unit_quantity', 'contenido_peso', 'is_on_promotion', 'discount_type', 'discount_value', 'final_price'
     ]
-    
-class UnitTypeAggregationAdmin(admin.ModelAdmin):
-    list_display = ('unit_type', 'name', 'conversion_factor')
-    list_filter = ('unit_type',)
 
-admin.site.register(Product, ProductAdmin)
+# Otros registros
 admin.site.register(Category)
 admin.site.register(Attribute, AttributeAdmin)
-admin.site.register(ProductVariation, ProductVariationAdmin)
 admin.site.register(UnitType)
-admin.site.register(UnitTypeAggregation, UnitTypeAggregationAdmin)
+admin.site.register(UnitTypeAggregation)
