65a2225d97d3d028ed4da4fb68981a11da2af4db
loocaldev <loocaldigital@gmail.com>
2024-11-16 10:32:26 -0500

    adjust otp 4

--- loocal/services.py
+++ loocal/services.py
@@ -19,7 +19,8 @@ def send_sms_otp(phone_number, otp_code):
     )
     
 def generate_otp():
-    return str(random.randint(100000, 999999))  # OTP de 6 dígitos.
+    # Genera un código OTP de 4 dígitos
+    return f"{random.randint(1000, 9999)}"
 
 def send_email_otp(email, otp_code):
     subject = 'Tu código de verificación'
