34f7cbfb3fa461926378a40efd02fcb93e87a1a5
loocaldev <loocaldigital@gmail.com>
2024-11-27 22:16:25 -0500

    adjust

--- orders/models.py
+++ orders/models.py
@@ -109,7 +109,7 @@ class Order(models.Model):
     subtotal = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    checkout = models.BooleanField(default=False)
+    # checkout = models.BooleanField(default=False)
     is_temporary = models.BooleanField(default=True)
     company = models.ForeignKey(
         Company, on_delete=models.SET_NULL, null=True, blank=True, related_name="orders"
